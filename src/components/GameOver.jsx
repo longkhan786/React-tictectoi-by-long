39ba5a1d95c81ab2547124b046ae6bc7fc0c3d0e
longkhan786 <longkhandhareja78@gmail.com>
2023-12-20 20:50:18 +0530

    3rd change

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,7 +2,7 @@ export default function GameOver({ winner, isClick }) {
     return (
         <div id="game-over">
             <h2>Game Over</h2>
-            {winner && <p>{winner} Won the Mach play again!</p>}
+            {winner && <p>{winner} Won the Match play again!</p>}
             {!winner && <p>Its Draw</p>}
             <button onClick={isClick}>Rematch</button>
         </div>
